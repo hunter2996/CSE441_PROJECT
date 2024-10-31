988fd2bc0d84982317cbec9c1b5e1c117c793b52
hunter2996 <NguyenThanhTung29603@gmail.com>
2024-10-31 22:48:04 +0700

    App khach hang

---
+++ appkhachhang/AppBanHang/app/src/main/java/com/example/appbanhang/activity/GioHangActivity.java
@@ -0,0 +1,124 @@
+package com.example.appbanhang.activity;
+
+import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appbanhang.R;
+import com.example.appbanhang.adapter.GioHangAdapter;
+import com.example.appbanhang.model.EventBus.TinhTongEvent;
+import com.example.appbanhang.utils.Utils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+public class GioHangActivity extends AppCompatActivity {
+
+    TextView giohangtrong, tongtien;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    Button btnmuahang;
+    GioHangAdapter adapter;
+    long tongtiensp;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_gio_hang);
+
+        initView();
+        initControl();
+        tinhTongTien();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        if(Utils.manggiohang.size() == 0){
+            giohangtrong.setVisibility(View.VISIBLE);
+        }else {
+            adapter = new GioHangAdapter(getApplicationContext(), Utils.manggiohang);
+            recyclerView.setAdapter(adapter);
+        }
+
+
+    }
+
+    private void tinhTongTien() {
+        tongtiensp = 0;
+        for(int i = 0; i<Utils.manggiohang.size(); i++){
+            tongtiensp = tongtiensp + (Utils.manggiohang.get(i).getGiasp() * Utils.manggiohang.get(i).getSoluong());
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        tongtien.setText(decimalFormat.format(tongtiensp));
+        //        tongtien.setText(tongtiensp+"");
+
+
+    }
+
+    private void initControl() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnmuahang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ThanhToanActivity.class);
+                intent.putExtra("tongtien", tongtiensp);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initView() {
+        giohangtrong = findViewById(R.id.txtgiohangtrong);
+        tongtien = findViewById(R.id.txttongtien);
+        toolbar = findViewById(R.id.toobar);
+        recyclerView = findViewById(R.id.recycleviewgiohang);
+        btnmuahang = findViewById(R.id.btnmuahang);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+    @Override
+    protected void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    public void eventTinhTien(TinhTongEvent event){
+        if(event != null){
+            tinhTongTien();
+        }
+    }
+
+}
